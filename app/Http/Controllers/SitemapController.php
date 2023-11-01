164dcdd85cb354a859b39fafb64f106a287866dc
Anthony REINA aka Toodle <anthonyreina.pro@gmail.com>
2023-11-01 21:59:30 +0100

    Fix bug on empty data (no crawl avalaible) SitemapController

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -9,21 +9,25 @@ class SitemapController extends Controller
 {   
     public function index () 
     {
-        $lastActiveCrawl = Crawls::latest('id')->first();
-        $hyperlinks = Hyperlinks::where('crawl_id', $lastActiveCrawl->id)->get();
-
         $tree = [];
-        foreach ($hyperlinks as $link) {
-            if (str_contains($link['hyperlink'], env('APP_NAME'))) {
-                if (str_contains($link['hyperlink'], '#')) {
-                    if (!array_key_exists('Home', $tree)) {
-                        $tree['Home'] = [];
+        $lastActiveCrawl = Crawls::latest('id')->first();
+
+        if ($lastActiveCrawl) {
+            $hyperlinks = Hyperlinks::where('crawl_id', $lastActiveCrawl->id)->get();
+
+        
+            foreach ($hyperlinks as $link) {
+                if (str_contains($link['hyperlink'], env('APP_NAME'))) {
+                    if (str_contains($link['hyperlink'], '#')) {
+                        if (!array_key_exists('Home', $tree)) {
+                            $tree['Home'] = [];
+                        }
+                        
+                        array_push($tree['Home'], ucfirst(explode('#', $link['hyperlink'])[1]));
+                    } else {
+                        $element = explode('/', $link['hyperlink']);
+                        $tree[ucfirst(end($element))] = [];
                     }
-                    
-                    array_push($tree['Home'], ucfirst(explode('#', $link['hyperlink'])[1]));
-                } else {
-                    $element = explode('/', $link['hyperlink']);
-                    $tree[ucfirst(end($element))] = [];
                 }
             }
         }
